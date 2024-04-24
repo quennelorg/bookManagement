f384dd80e50199ca2eeddbb117e5175895aea818
xinyuji <quennel1115@gmail.com>
2024-04-24 18:38:25 +0800

    feat: intial ios project

---
+++ frontend/ios/BookManagement/BookManagement/BookManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookManagementApp.swift
+//  BookManagement
+//
+//  Created by Xinyu Ji on 2024/4/24.
+//
+
+import SwiftUI
+
+@main
+struct BookManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
