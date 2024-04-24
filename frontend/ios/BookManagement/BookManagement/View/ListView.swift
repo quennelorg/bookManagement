22b2d009d93cb583768eae5f3bac840aaff5d19a
xinyuji <quennel1115@gmail.com>
2024-04-24 18:48:00 +0800

    feat: add listview

--- frontend/ios/BookManagement/BookManagement/View/ListView.swift
+++ frontend/ios/BookManagement/BookManagement/View/ListView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct ListView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List(mockBook, id:\.id) { item in
+            ListItemView(book: item)
+        }
     }
 }
 
