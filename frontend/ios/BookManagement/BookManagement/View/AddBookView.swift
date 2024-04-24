47955a57410c4ee380fbc8993865c88fcfbefacc
xinyuji <quennel1115@gmail.com>
2024-04-24 19:00:37 +0800

    feat: add navigation for add book

--- frontend/ios/BookManagement/BookManagement/View/AddBookView.swift
+++ frontend/ios/BookManagement/BookManagement/View/AddBookView.swift
@@ -23,16 +23,35 @@ struct AddBookView: View {
     }
     
     var body: some View {
-        List {
-            Section(header: Text("Book detail")) {
-                TextField("Please input Book title", text: $newBook.title)
-                TextField("Please input Book author", text: $newBook.author)
-                TextField("Please input Book ISBN", text: $newBook.isbn)
-            }
-            Section(header: Text("Book PublishedDate")) {
-                DatePicker("Published Date", selection: bindingDate, displayedComponents: [.date])
-                            }
+        NavigationStack {
+            List {
+                Section(header: Text("Book detail")) {
+                    TextField("Please input Book title", text: $newBook.title)
+                    TextField("Please input Book author", text: $newBook.author)
+                    TextField("Please input Book ISBN", text: $newBook.isbn)
+                }
+                Section(header: Text("Book PublishedDate")) {
+                    DatePicker("Published Date", selection: bindingDate, displayedComponents: [.date])
+                }
+            }.navigationTitle("Add Book")
+                .toolbar{
+                    ToolbarItem(placement: .topBarLeading) {
+                        Button {
+                            print("cancel")
+                        } label: {
+                            Text("Cancel")
+                        }
+                    }
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button {
+                           print("add")
+                        } label: {
+                            Text("Add")
+                        }.disabled(newBook.title.isEmpty)
+                    }
+                }
         }
+        
     }
 }
 
